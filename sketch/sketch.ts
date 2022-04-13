3861a2a63623375697fbd0bdd1fda41faf1aa7af
nbogie <neillbogie@googlemail.com>
2022-04-13 12:10:15 +0100

    improved vis

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -6,7 +6,21 @@ function setup() {
 function draw() {
   const circlesArray = calculatePackedCircles(width, height);
   for (const c of circlesArray) {
-    fill('orange')
-    circle(c.pos.x, c.pos.y, c.radius * 2);
+    drawCircle(c);
   }
+}
+
+function drawCircle(c: Circle) {
+  fill(randomColour());
+  noStroke();
+  circle(c.pos.x, c.pos.y, c.radius * 2);
+}
+
+function randomColour(): p5.Color {
+  push();
+  colorMode(HSB);
+  //color(hue is 0-360, saturation is 0-100, brightness is 0-100)
+  const c = color(random(20, 50), random(80, 100), 100);
+  pop();
+  return c;
 }
