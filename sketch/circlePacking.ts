fb7be096a08ae8dca2ff301ad78bc4f07222c27d
nbogie <neillbogie@googlemail.com>
2022-04-13 11:16:57 +0100

    split out circle packing calculations from p5 vis

---
+++ sketch/circlePacking.ts
@@ -0,0 +1,26 @@
+interface Circle {
+    pos: Position;
+    radius: number;
+}
+interface Position {
+    x: number;
+    y: number;
+}
+
+function calculatePackedCircles(): Circle[] {
+    //TODO: you need to implement this function properly!
+    return [
+        { pos: { x: 300, y: 300 }, radius: 100 },
+        { pos: { x: 100, y: 100 }, radius: 20 },
+    ];
+}
+
+/** Returns the distance between two given positions.
+    This function doesn't require p5.js 
+ */
+function distance(p1: Position, p2: Position): number {
+    const x = p1.x - p2.x;
+    const y = p1.y - p2.y;
+    const hyp = Math.sqrt(x * x + y * y);
+    return hyp;
+}
--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,12 +1,3 @@
-interface Circle {
-  pos: Position;
-  radius: number;
-}
-interface Position {
-  x: number;
-  y: number;
-}
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noLoop();
@@ -18,12 +9,4 @@ function draw() {
     fill('orange')
     circle(c.pos.x, c.pos.y, c.radius * 2);
   }
-}
-
-function calculatePackedCircles(): Circle[] {
-  //TODO: you need to implement this function properly!
-  return [
-    { pos: { x: 300, y: 300 }, radius: 100 },
-    { pos: { x: 100, y: 100 }, radius: 20 },
-  ];
 }
