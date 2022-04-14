1d20e9915f9615c2f9dffdb19c1570366ce4a233
Zahra <bghzahra@outlook.com>
2022-04-14 12:52:11 +0100

    Final circle packing completed

--- sketch/circlePacking.ts
+++ sketch/circlePacking.ts
@@ -8,12 +8,53 @@ interface Position {
 }
 
 function calculatePackedCircles(areaWidth: number, areaHeight: number): Circle[] {
-    //TODO: you need to implement this function properly!
-    return [
-        { pos: { x: 300, y: 300 }, radius: 100 },
-        { pos: { x: 100, y: 100 }, radius: 20 },
-    ];
+    let validatedCircles = []
+    for (let i = 0; i < 100000; i++) {
+        let candidate = generateRandomPos()
+        if (doesCirlceOverlapAll(validatedCircles, candidate)) {
+            continue
+        } else {
+            validatedCircles.push(candidate)
+        }
+    }
+    return validatedCircles
 }
+
+function generateRandomPos() {
+    let validatedCircles = { pos: { x: randomInt(windowWidth), y: randomInt(windowHeight) }, radius: randomInt(100) }
+    return validatedCircles
+}
+
+function randomInt(max: number) {
+    return Math.floor(Math.random() * max)
+}
+
+function doCirclesOverlap(circle1: Circle, circle2: Circle):boolean {
+    let distanceBetween = distance(circle1.pos, circle2.pos)
+    if (distanceBetween < circle1.radius + circle2.radius) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function test() {
+    console.log(doCirclesOverlap({ pos: { x: 0, y: 0 }, radius: 100 }, { pos: { x: 0, y: 0 }, radius: 100 }))
+    console.log(doCirclesOverlap({ pos: { x: 0, y: 0 }, radius: 100 }, { pos: { x: 301, y: 0 }, radius: 200 }))
+    console.log(doCirclesOverlap({ pos: { x: 0, y: 0 }, radius: 100 }, { pos: { x: 299, y: 0 }, radius: 200 }))
+}
+
+
+
+function doesCirlceOverlapAll(arrayOfCirles: Circle[], circleToCheck: Circle):boolean {
+    for (let circle of arrayOfCirles) {
+        if (doCirclesOverlap(circle, circleToCheck)) {
+            return true
+        }
+    } 
+    return false
+}
+
 
 /** Returns the distance between two given positions.
     This function doesn't require p5.js 
